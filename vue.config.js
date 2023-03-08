28a8eaf100658571547e960cf9731100d6f67b1c
noworktoday00 <noworktoday00@gmail.com>
2023-03-08 12:10:49 +0800

    change dist path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/green-court/dist'
+  publicPath: '/green-court/dist/'
 });
