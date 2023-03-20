1ec13c95950e108b6bff0d615e196a5e26f79e79
noworktoday00 <noworktoday00@gmail.com>
2023-03-20 14:08:36 +0800

    付款頁面調整

--- src/views/frontPage/CheckOrder.vue
+++ src/views/frontPage/CheckOrder.vue
@@ -1,67 +1,77 @@
 <template>
-  <div class="my-48 row justify-content-center">
-    <form action="" class="col-md-6" @submit.prevent="payOrder">
-      <table class="table align-middle">
-        <thead>
-          <th>品名</th>
-          <th>數量</th>
-          <th>單價</th>
-        </thead>
-        <tbody>
-          <tr v-for="item in order.products" :key="item.id">
-            <td> {{ item.product.title }} </td>
-            <td> {{item.qty}} /{{item.product.unit}}</td>
-            <td> {{ item.product.price }} </td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>總計</td>
-            <td> {{ order.total }} </td>
-          </tr>
-        </tfoot>
-      </table>
-      <table class="table">
-        <tbody>
-          <tr>
-            <th>姓名</th>
-            <td>{{ order.user.name }}</td>
-          </tr>
-          <tr>
-            <th>Email</th>
-            <td>{{ order.user.email }}</td>
-          </tr>
-          <tr>
-            <th>電話</th>
-            <td>{{ order.user.tel }}</td>
-          </tr>
-          <tr>
-            <th>地址</th>
-            <td>{{ order.user.address }}</td>
-          </tr>
-          <tr>
-            <th>付款狀態</th>
-            <td>
-              <span v-if="!order.is_paid" class="text-danger">尚未付款</span>
-              <span v-else class="text-success">付款完成</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div v-if="order.is_paid === false" class="text-end">
-        <button class="btn btn-outline-danger">確認付款</button>
-      </div>
-      <div v-else class="text-end">
-        <router-link to="/activities" class="btn btn-outline-primary">
-          查看其他球團活動
-        </router-link>
-      </div>
-    </form>
+  <ProductBanner />
+  <div class="gradient-bg">
+    <div class="py-48 row justify-content-center">
+      <form action="" class="col-md-6" @submit.prevent="payOrder">
+        <table class="table align-middle text-white">
+          <thead>
+            <th>日期</th>
+            <th>品名</th>
+            <th>人數</th>
+            <th>單價</th>
+          </thead>
+          <tbody>
+            <tr v-for="item in order.products" :key="item.id">
+              <td>{{ item.product.date }} / {{ item.product.time }}</td>
+              <td> {{ item.product.title }} </td>
+              <td> {{item.qty}} / 人</td>
+              <td> {{ item.product.price }} </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>總計</td>
+              <td> {{ order.total }} </td>
+            </tr>
+          </tfoot>
+        </table>
+        <table class="table text-white">
+          <tbody>
+            <tr>
+              <th>姓名</th>
+              <td>{{ order.user.name }}</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>{{ order.user.email }}</td>
+            </tr>
+            <tr>
+              <th>電話</th>
+              <td>{{ order.user.tel }}</td>
+            </tr>
+            <tr>
+              <th>地址</th>
+              <td>{{ order.user.address }}</td>
+            </tr>
+            <tr>
+              <th>付款狀態</th>
+              <td>
+                <span v-if="!order.is_paid" class="text-danger">尚未付款</span>
+                <span v-else class="text-success">付款完成</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div v-if="order.is_paid === false" class="text-end">
+          <button class="btn btn-outline-danger">確認付款</button>
+        </div>
+        <div v-else class="text-end">
+          <router-link to="/activities" class="btn btn-outline-primary">
+            查看其他球團活動
+          </router-link>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import ProductBanner from '@/components/ProductBanner.vue';
+
 export default {
+  components: {
+    ProductBanner,
+  },
   data() {
     return {
       order: {
